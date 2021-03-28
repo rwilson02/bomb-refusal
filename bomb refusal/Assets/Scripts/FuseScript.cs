f41be5fd45f2e7a38fbab92ba07ee564e49e0b14
Rory <rwilson2@ltu.edu>
2021-03-28 00:48:36 -0400

    procrastination is my strong suit
    i wish it wasn't

---
+++ bomb refusal/Assets/Scripts/FuseScript.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FuseScript : MonoBehaviour
+{
+    public Animator burnings;
+    public KeyCode DebugIN, DebugOUT;
+    GameObject connection;
+
+    public GameObject connector;
+    ConnectorScript connectorScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        burnings = gameObject.GetComponent<Animator>();
+        connector = gameObject.transform.GetChild(0).gameObject;
+        connectorScript = connector.GetComponent<ConnectorScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(connection != null)
+        {
+            CheckConnect();
+        }
+
+        //debug commands
+        {
+            if (Input.GetKeyDown(DebugIN))
+            {
+                if (burnings != null)
+                {
+                    burnings.ResetTrigger("burnIn");
+                    burnings.ResetTrigger("burnOut");
+                }
+                StartBurning(true);
+            }
+
+            if (Input.GetKeyDown(DebugOUT))
+            {
+                if (burnings != null)
+                {
+                    burnings.ResetTrigger("burnIn");
+                    burnings.ResetTrigger("burnOut");
+                }
+                StartBurning(false);
+            }
+        }
+    }
+
+    public void StartBurning(bool inward)
+    {
+        if (burnings != null)
+        {
+            if (inward)
+            {
+                burnings.SetTrigger("burnIn");
+            }
+            else
+            {
+                burnings.SetTrigger("burnOut");
+            }
+        }
+    }
+
+    public void CheckConnect()
+    {
+        connection = connectorScript.connected;
+    }
+}
---
+++ bomb refusal/Assets/Scripts/TileTurner.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileTurner : MonoBehaviour
+{
+    Camera cam;
+    public GameObject hitTile;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+
+        //randomize orientation
+
+        //quick refresh
+        gameObject.transform.Translate(0, 0, -0.5f);
+        gameObject.transform.Translate(0, 0, 0.5f);
+    }
+
+    private void Update()
+    {
+        if (Clicked() > -1)
+        {
+            int dir = Clicked();
+            RaycastHit2D hit = Physics2D.Raycast(cam.ScreenToWorldPoint(Input.mousePosition), Vector3.zero);
+
+            if(hit.collider != null && hit.collider.CompareTag("Tile"))
+            {
+                Transform tile = hit.transform;
+                var eul = tile.eulerAngles;
+
+                gameObject.transform.Translate(0, 0, -0.5f);
+                if (dir == 0)
+                {
+                    eul.z += 90;
+                }
+                else
+                {
+                    eul.z -= 90;
+                }
+
+                tile.eulerAngles = eul;
+                gameObject.transform.Translate(0, 0, 0.5f);
+            }
+        }
+    }
+
+    private int Clicked()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            return 0;
+        }
+        else if (Input.GetMouseButtonDown(1))
+        {
+            return 1;
+        }
+        else return -1;
+    }
+}
